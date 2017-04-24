73346dafb7148492bc0821b38abb4526952018be
ipiao <yukk046@zhixubao.com>
2017-04-24 14:54:49 +0800

    temp

--- medb/db_test.go
+++ medb/db_test.go
@@ -7,12 +7,32 @@ import (
 )
 
 func TestMeDB(t *testing.T) {
-	var datasource = "root:1001@tcp(127.0.0.1:3306)/test?charset=utf8mb4&loc=Asia%2fShanghai"
-	err := RegisterDB("test", "mysql", datasource)
+	var datasource = "ipiao:1001@tcp(192.168.1.201:3306)/web_from_pg?charset=utf8mb4&loc=Asia%2fShanghai"
+	err := RegisterDB("web_from_pg", "mysql", datasource)
 	if err != nil {
 		t.Fatal(err)
 	}
-	db := OpenDB("test")
+	db := OpenDB("web_from_pg")
 	err = db.Ping()
-    t.
+	t.Log(err == nil)
+	for i := 0; i < 100; i++ {
+		rows := db.Prepare("select * from consignor_user").Query()
+		defer rows.Close()
+	}
+}
+
+func BenchmarkMeDB(b *testing.B) {
+	var datasource = "ipiao:1001@tcp(192.168.1.201:3306)/web_from_pg?charset=utf8mb4&loc=Asia%2fShanghai"
+	err := RegisterDB("web_from_pg", "mysql", datasource)
+	if err != nil {
+		b.Fatal(err)
+	}
+	db := OpenDB("web_from_pg")
+	err = db.Ping()
+	b.Log(err == nil)
+
+	for i := 0; i < b.N; i++ {
+		rows := db.Prepare("select * from consignor_user").Query()
+		defer rows.Close()
+	}
 }
