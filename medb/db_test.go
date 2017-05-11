0a93c8f2ab0ae36036508571a2f62569fe6f87c1
ipiao <yukk046@zhixubao.com>
2017-05-11 09:57:27 +0800

    tx test

--- medb/db_test.go
+++ medb/db_test.go
@@ -1,7 +1,40 @@
 package medb
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func TestDB(t *testing.T) {
-	// db := RegisterDB("test", "mysql", "root:1001@tcp(127.0.0.1:3306)/test?charset=utf8mb4&loc=Asia%2fShanghai")
+	err := RegisterDB("test", "mysql", "root:1001@tcp(127.0.0.1:3306)/test?charset=utf8mb4&loc=Asia%2fShanghai")
+	if err != nil {
+		t.Fatal("register db err:", err)
+	}
+	db := OpenDB("test")
+	tx1, err := db.Begin()
+	if err != nil {
+		t.Fatal("tx1 begin err:", err)
+	}
+	tx1.Exec("insert into user(name)values('tx1')").Err()
+
+	go func(db *DB) {
+		tx2, err := db.Begin()
+		if err != nil {
+			t.Fatal("tx2 begin err:", err)
+		}
+		tx2.Exec("insert into user(name)values('tx2')").Err()
+
+		tx2.Commit()
+	}(db)
+
+	err = tx1.Exec("insert into user(name1)values('tx3')").Err()
+
+	err = tx1.Exec("insert into user(name)values('tx4')").Err()
+
+	time.Sleep(time.Second)
+	err = tx1.Commit()
+
+	t.Fatal("commit  err", err)
 }
